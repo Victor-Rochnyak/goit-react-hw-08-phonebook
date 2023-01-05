4fbf921f5aea153675762e10d87c0efc4f652bea
Victor-Rochnyak <victor.rochnyak@gmail.com>
2023-01-05 13:08:12 +0200

    .

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -3,7 +3,7 @@ import { HomePageBox,Img } from './Home.styled';
 export default function Home() {
   return (
     <HomePageBox>
-      <img src="https://img.logoipsum.com/245.svg" />
+      <Img src="https://img.logoipsum.com/245.svg" />
       <h1>
         Welcome to Phonebook
       </h1>
